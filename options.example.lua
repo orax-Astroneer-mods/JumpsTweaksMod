cb1c1be724b6c95106c31e26e224c848ad89d403
orax <0-F@users.noreply.github.com>
2025-04-29 19:35:32 +0200

    create create-release.yml

--- options.example.lua
+++ options.example.lua
@@ -1,14 +1,43 @@
 --[[
 Commented lines are ignored. If you don't want to change a value, comment out the line.
 "A comment starts anywhere with a double hyphen (--) and runs until the end of the line." https://www.lua.org/pil/1.3.html
+]]
 
+--[[
+Game defaults:
+    AirControlDampening = 0.3
+    JumpBoostImpulse = 0.0
+    JumpVerticalImpulse = 900.0
+    MinFallingDamageVelocity = 1650.0
+    MaxFallingDamageVelocity = 2950.0
 ]]
 
 ---@type JumpTweaksMod_Options
 return {
-    AirControlDampening = 0.6,       -- 0.3
-    JumpBoostImpulse = 0,            -- 0.0
-    JumpVerticalImpulse = 1800,      -- 900.0
-    -- MinFallingDamageVelocity = 1650, -- 1650.0
-    MaxFallingDamageVelocity = 5900, -- 2950.0
+    -- The first preset is the one that is loaded when you start the game.
+    -- You can add more presets.
+    presets = {
+        {
+            AirControlDampening = 0.6,
+            -- JumpBoostImpulse = 0,
+            JumpVerticalImpulse = 1800,
+            -- MinFallingDamageVelocity = 1650,
+            MaxFallingDamageVelocity = 5900,
+        },
+        {
+            AirControlDampening = 0.3,
+            -- JumpBoostImpulse = 0,
+            JumpVerticalImpulse = 900,
+            -- MinFallingDamageVelocity = 1650,
+            -- MaxFallingDamageVelocity = 2950,
+        },
+    },
+    -- Press CTRL+SPACE to switch between presets.
+    -- To disable this keybind, comment out the lines below.
+    -- Key-code strings: https://docs.ue4ss.com/dev/lua-api/table-definitions/key.html
+    -- Modifier key-code strings: https://docs.ue4ss.com/dev/lua-api/table-definitions/modifierkey.html
+    keybinds = {
+        key = Key.SPACE,
+        modifierKeys = { ModifierKey.CONTROL }
+    }
 }
